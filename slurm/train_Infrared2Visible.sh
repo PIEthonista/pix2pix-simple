e476d94d4d16939edb3300995784b1a1e3624f84
PIEthonista <gohyixian4546@gmail.com>
2023-09-25 00:25:43 +0800

    lowered traininb batch size from 32 to 2

--- slurm/train_Infrared2Visible.sh
+++ slurm/train_Infrared2Visible.sh
@@ -10,4 +10,4 @@
 #SBATCH --mail-user=gohyixian456@gmail.com
 
 # 520550
-python train.py --root_dir /work/u5832291/yixian/pix2pix_simple --project_name pix2pix_Infrared2Visible_2 --wandb_project palette_scene2scene --train_dataset_input_dir /work/u5832291/datasets/LLVIP/infrared/train --train_dataset_target_dir /work/u5832291/datasets/LLVIP/visible/train --test_dataset_input_dir /work/u5832291/datasets/LLVIP/infrared/test --test_dataset_target_dir /work/u5832291/datasets/LLVIP/visible/test --batch_size 32 --test_batch_size 1 --direction a2b --gpu_id 0 
+python train.py --root_dir /work/u5832291/yixian/pix2pix_simple --project_name pix2pix_Infrared2Visible_2 --wandb_project palette_scene2scene --train_dataset_input_dir /work/u5832291/datasets/LLVIP/infrared/train --train_dataset_target_dir /work/u5832291/datasets/LLVIP/visible/train --test_dataset_input_dir /work/u5832291/datasets/LLVIP/infrared/test --test_dataset_target_dir /work/u5832291/datasets/LLVIP/visible/test --batch_size 2 --test_batch_size 1 --direction a2b --gpu_id 0 
